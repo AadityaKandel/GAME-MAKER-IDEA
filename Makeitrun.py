54d6813cf24219c06f44b1b04fa11601236df598
AK HACKS OR TRICKS <71229713+AadityaKandel@users.noreply.github.com>
2020-11-24 19:39:16 +0545

    Create Makeitrun.py

---
+++ Makeitrun.py
@@ -0,0 +1,14 @@
+import os
+import time
+
+print('''Just Wait :):)''')
+
+os.system('pip install pygame')
+
+os.system('cls')
+
+print(''' Now Just Open The FIle And Run It :) :) :) :) :)''')
+
+print('Press Any Key TO Continue')
+
+a = input()
